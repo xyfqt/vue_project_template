7fe69281456161dfad9846e8326990b717d7fe59
qingpengtan <tang1994year163.com>
2020-04-20 15:57:13 +0800

    路由修改

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from './router'
-import store from './store'
+import router from './router/index'
+import store from './store/index'
 import './utils/htmlFontSize'
 import './assets/styles/global.scss'
 import "@/utils/MostonePayApiT";
--- src/router/index.js
+++ src/router/index.js
@@ -4,20 +4,27 @@ import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
-const routes = [
+const routes =  [
   {
     path: '/',
-    name: 'Home',
+    name: 'home',
     component: Home
   },
   {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
+    path: '/success',
+    name: 'Success',
+    component: () => import( /* webpackChunkName: "Success" */ '../views/Success.vue')
+  },
+  {
+    path: '/test',
+    name: 'Test',
+    component: () => import( /* webpackChunkName: "Test" */ '../views/Test.vue')
+  },
+  {
+    path: '/ad/suim',
+    name: 'SuiMing',
+    component: () => import( /* webpackChunkName: "SuiMing" */ '../views/ad/SuiMing.vue')
+  },
 ]
 
 const router = new VueRouter({
